0ab377c5f6c53aa227b165c8bbd88d69ea18144d
wenyu <wenyutech@foxmail.com>
2022-06-18 21:24:48 +0800

    update extensions

--- util.go
+++ util.go
@@ -22,42 +22,42 @@ func GetImageType(bytes []byte) (ext string, mime string, decoder func(r io.Read
     }
 
     if bytes[0] == 0xFF && bytes[1] == 0xD8 {
-        ext = ".jpg"
+        ext = "jpg"
         mime = "image/jpeg"
         decoder = jpeg.Decode
     }
 
     if len(bytes) >= 4 && bytes[0] == 0x89 && bytes[1] == 0x50 && bytes[2] == 0x4E && bytes[3] == 0x47 {
-        ext = ".png"
+        ext = "png"
         mime = "image/png"
         decoder = png.Decode
     }
 
     if bytes[0] == 0x42 && bytes[1] == 0x4D {
-        ext = ".bmp"
+        ext = "bmp"
         mime = "image/x-ms-bmp"
         decoder = bmp.Decode
     }
 
     if (bytes[0] == 0x49 && bytes[1] == 0x49) || (bytes[0] == 0x4D && bytes[1] == 0x4D) {
-        ext = ".tiff"
+        ext = "tiff"
         mime = "image/tiff"
         decoder = tiff.Decode
     }
 
     if bytes[0] == 0x52 && bytes[1] == 0x49 {
-        ext = ".webp"
+        ext = "webp"
         mime = "image/webp"
         decoder = webp.Decode
     }
 
     /*if bytes[0] == 0x47 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x38 {
-          ext = ".gif"
+          ext = "gif"
           mime = "image/gif"
       }
 
       if bytes[0] == 0x00 && bytes[1] == 0x00 && (bytes[2] == 0x01 || bytes[2] == 0x02) && bytes[3] == 0x00 {
-          ext = ".ico"
+          ext = "ico"
           mime = "image/x-icon"
       }*/
 
